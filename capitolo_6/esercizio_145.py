0b5e3b973a4852e9010b8b7119b1ada378d0c971
andy90-star <lampifuo89@gmail.com>
2021-01-06 17:24:04 +0100

    Add file 146

--- capitolo_6/esercizio_145.py
+++ capitolo_6/esercizio_145.py
@@ -1,5 +1,49 @@
+#!/bin/python3
+def point(word, contator):
+    dicto_point = {
+    "A" : 1,
+    "B" : 3,
+    "C" : 3,
+    "D" : 2,
+    "E" : 1,
+    "F" : 4,
+    "G" : 2,
+    "H" : 4,
+    "I" : 1,
+    "J" : 8,
+    "K" : 5,
+    "L" : 1,
+    "M" : 3,
+    "N" : 1,
+    "O" : 1,
+    "P" : 3,
+    "Q" : 10,
+    "R" : 1,
+    "S" : 1,
+    "T" : 1,
+    "U" : 1,
+    "V" : 4,
+    "W" : 4,
+    "X" : 8,
+    "Y" : 4,
+    "Z" : 10,
+    }
+    lista_word = list(word)
+    for index_word in lista_word:
+        if index_word in dicto_point:
+            contator += dicto_point.get(index_word)
+    print("score: " + str(contator))
+
+
+
 def main():
-    pass
+    try:
+        contator = 0
+        word = input("Enter the name ").upper()
+        point(word, contator)
+    except ValueError or TypeError as error:
+        print(error)
+        
 
 if __name__ == '__main__':
     main()
---
+++ capitolo_6/esercizio_146.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+import random
+from tabulate import tabulate 
+def bingo():
+    index = 0
+    men = 1
+    add = 0
+    lista_new = []
+    dicto_bingo = {}
+    lista_bingo = ["b","i","n","g","o"]
+    for index_letter in lista_bingo:
+        add += 15
+        index_letter = list()
+        for index_number in range(0, 5):
+            moment = random.randint(men,add)
+            index_letter.append(moment)
+        print(tabulate([[str(lista_bingo[index]).upper()], [str(index_letter)]], headers=["Letter:"],  tablefmt='orgtbl'))
+        print("------------------------------------------")
+        men += 15
+        index += 1
+        
+def main():
+    bingo()
+
+if __name__ == '__main__':
+    main()
