0e5ea33ab70e1a34679e1814592c7fc61bc554ab
andy-90/star <lampifuo89@gmail.com>
2021-01-25 14:42:39 +0100

    update 129, add:130

--- capitolo_5/esercizio_129.py
+++ capitolo_5/esercizio_129.py
@@ -1,12 +1,8 @@
 #!/bin/python3
 def token(tok):
-    list_segn = ["-","*","/","+"]
     list_token = list(tok)
     for index_2 in list_token:
         index_2 = str(index_2)
-        for index in list_segn:
-            if index_2 == index:
-                list_token.remove(index_2)
     return list_token
         
 def main():
--- capitolo_5/esercizio_130.py
+++ capitolo_5/esercizio_130.py
@@ -1,5 +1,35 @@
-def main():
-    pass
+#!/bin/python3
+from esercizio_129 import token
+list_operator = ["-","+","*","/","("]
+f"""A +
+or - operator is unary if it is the first token in the list, or if the token that imme-
+diately precedes it is an operator or open parenthesis. Otherwise the operator is
+binary."""
+def unary_or_binary(expression, first):
+    if first[0] == "-" :
+        first.pop(0)
+        first.insert(0, "-u")
+    elif  first[0] == "+":
+        first.pop(0)
+        first.insert(0, "+u")
+    for index in range(1, len(first)-1):
+            for operator in list_operator:
+                if first[index-1] == operator and first[index] == "-":
+                    first.pop(index)
+                    first.insert(index, "-u")
+                if first[index-1] == operator and first[index] == "+":
+                    first.pop(index)
+                    first.insert(index, "+u")
+    final= "".join(first)
+    return final
 
+
+
+
+def main():
+    expression = input("Enter the expression mathematical: ")
+    first = token(expression)
+    first_1 = unary_or_binary(expression, first)
+    print(first_1)
 if __name__ == "__main__":
     main()
