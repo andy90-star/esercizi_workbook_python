5d89d8d8b40ba8f5c5bfec08f6923055c4361b30
andy-90/star <lampifuo89@gmail.com>
2021-01-26 17:02:03 +0100

    update:166

--- capitolo_7/esercizio_166.py
+++ capitolo_7/esercizio_166.py
@@ -1,13 +1,13 @@
 #!/bin/python3
 import os
-def list_name():
+def list_name(file_1, file_2):
     name=""
     list_final = list()
     list_final_girls = list()
     list_boy = list()
     list_girl = list()
-    file_1 =os.path.abspath("name_m.txt")
-    file_2 =os.path.abspath("name_f.txt")
+    file_1_1 =os.path.abspath(file_1)
+    file_2_2 =os.path.abspath(file_2)
     file_boy_open = open(file_1, "r")
     file_girl_open = open(file_2, "r")
     for letter_2 in file_boy_open:
@@ -41,7 +41,9 @@ def list_name():
     return list_final, list_final_girls
 
 def main():
-    first = list_name()
+    file_1 = input("Enter the male file: ")
+    file_2 = input("Enter the male file: ")
+    first = list_name(file_1, file_2)
     print(first)
 
 if __name__ == '__main__':
